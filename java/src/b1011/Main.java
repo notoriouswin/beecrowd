de7a068d7e85d152c9cbadeae1c1d34ec78ad0c4
notoriouswin <bessadev13@gmail.com>
2026-07-27 00:21:55 -0300

    Realizando alguns desafios beedcrow

---
+++ java/src/b1011/Main.java
@@ -0,0 +1,17 @@
+package b1011;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static final double NUMERO_DE_PI = 3.14159;
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        double raio = scan.nextDouble();
+        double volume = (4f / 3f)  * NUMERO_DE_PI * Math.pow(raio,3);
+
+        System.out.printf("VOLUME = %.3f%n",volume);
+    }
+}
