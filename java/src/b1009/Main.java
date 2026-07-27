25f32e56fa549110fc3f5ac93a1e0b2d211c00a5
notoriouswin <bessadev13@gmail.com>
2026-07-27 00:03:24 -0300

    Realizando alguns desafios beedcrow

--- java/src/b1009/Main.java
+++ java/src/b1009/Main.java
@@ -1,4 +1,19 @@
 package b1009;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        String nome = scan.next();
+        double salarioBase = scan.nextDouble();
+        double venda = scan.nextDouble();
+        double comissao = venda * 0.15;
+        double totalSalario = salarioBase + comissao;
+
+        System.out.printf("TOTAL = R$ %.2f%n",totalSalario);
+    }
 }
